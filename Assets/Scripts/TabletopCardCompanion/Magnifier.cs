2fb070d01f660aefaedf44e95c0f28a3b2e7bb3e
Kevin O'Mara <kevin.d.omara@gmail.com>
2018-04-16 22:28:07 -0700

    Add basic magnifier UI element

---
+++ Assets/Scripts/TabletopCardCompanion/Magnifier.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TabletopCardCompanion
+{
+    /// <summary>
+    /// UI element that presents an enlarged sprite for easier viewing.
+    /// </summary>
+    [RequireComponent(typeof(Image))]
+    public class Magnifier : MonoBehaviour
+    {
+        /// <summary>
+        /// Set which sprite is magnified.
+        /// </summary>
+        public void SetSprite(Sprite sprite)
+        {
+            image.sprite = sprite;
+            image.color = Color.white;
+        }
+
+        /// <summary>
+        /// Clear the currently magnified sprite.
+        /// </summary>
+        public void ClearSprite()
+        {
+            image.sprite = null;
+            image.color = Color.clear;
+        }
+
+
+        private Image image;
+
+        private void Awake()
+        {
+            image = GetComponent<Image>();
+        }
+    }
+}
