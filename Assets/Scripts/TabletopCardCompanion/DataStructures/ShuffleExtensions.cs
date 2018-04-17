a5d99b08c47033eb5ce9c2bc13f59bdf3ca52257
Kevin O'Mara <kevin.d.omara@gmail.com>
2018-04-17 00:53:15 -0700

    Fix bug in Fischer-Yate shuffle

--- Assets/Scripts/TabletopCardCompanion/DataStructures/ShuffleExtensions.cs
+++ Assets/Scripts/TabletopCardCompanion/DataStructures/ShuffleExtensions.cs
@@ -20,7 +20,7 @@ namespace TabletopCardCompanion.DataStructures
         {
             for (int i = list.Count - 1; i > 0; --i)
             {
-                var j = UnityEngine.Random.Range(0, i);
+                var j = UnityEngine.Random.Range(0, i + 1);
 
                 var tmp = list[j];
                 list[j] = list[i];
