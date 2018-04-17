94528c3be9907596aff5a911a2f4277888c995ac
Kevin O'Mara <kevin.d.omara@gmail.com>
2018-04-17 00:38:10 -0700

    Add intercepter for next tap after magnifying
    When a sprite is magnified, the next single tap is intercepted. This tap
    tells the magnifier to stop showing the enlarged image.

--- Assets/Scripts/TabletopCardCompanion/MagnifierController.cs
+++ Assets/Scripts/TabletopCardCompanion/MagnifierController.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TabletopCardCompanion.PlayingPieces.Components;
+using TouchScript.Gestures;
+using TouchScript.Layers;
 using UnityEngine;
 
 namespace TabletopCardCompanion
@@ -8,30 +11,54 @@ namespace TabletopCardCompanion
     /// <summary>
     /// Connector pattern. Routes requests to be magnified to the correct magnifier. Controls logic regarding when to start/stop magnifying.
     /// </summary>
+    [RequireComponent(typeof(FullscreenLayer))]
+    [RequireComponent(typeof(TapGesture))]
     public class MagnifierController : MonoBehaviour
     {
         /// <summary>
-        /// Magnify the sprite that has requested to be enlarged.
+        /// Show an enlarged copy of the sprite. Enable the fullscreen layer so that the next touch is intercepted.
         /// </summary>
-        private void RequestMagnifyHandler(object sender, Sprite sprite)
+        public void Magnify(Sprite sprite)
         {
             magnifier.SetSprite(sprite);
+            fullscreenLayer.enabled = true;
         }
 
-        // TODO: After setting sprite, catch next tap. On that tap, clear the magnified sprite.
+        /// <summary>
+        /// Stop showing an enlarged sprite. Disable the fullscreen layer to stop intercepting touches.
+        /// </summary>
+        public void StopMagnifying()
+        {
+            magnifier.ClearSprite();
+            fullscreenLayer.enabled = false;
+        }
 
-        [SerializeField] private Magnifier magnifier;
+
+        [SerializeField]
+        private Magnifier magnifier;
+        private FullscreenLayer fullscreenLayer;
+
+        private void Awake()
+        {
+            fullscreenLayer = GetComponent<FullscreenLayer>();
+        }
 
         private void OnEnable()
         {
-            // TODO: Find a less coupled / omniscient way of doing this.
+            GetComponent<TapGesture>().Tapped += TappedHandler;
+
             // Subscribe to all classes that want to be magnified.
             DrawCardController.RequestMagnifyCard += RequestMagnifyHandler;
         }
 
         private void OnDisable()
         {
+            GetComponent<TapGesture>().Tapped -= TappedHandler;
+
             DrawCardController.RequestMagnifyCard -= RequestMagnifyHandler;
         }
+
+        private void TappedHandler(object sender, EventArgs e) { StopMagnifying(); }
+        private void RequestMagnifyHandler(object sender, Sprite sprite) { Magnify(sprite); }
     }
 }
